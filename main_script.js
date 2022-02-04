2ea697b4901c89ed85c741d70291aa6ed3fcbf73
zivserphos <zivfromisrael@gmail.com>
2022-02-04 11:12:31 +0200

    changes

--- main_script.js
+++ main_script.js
@@ -2,28 +2,28 @@ const path = require("path");
 const fs = require("fs");
 
 let totalQuestionsInsert = 0;
-
 const files = fs.readdirSync("./questions");
 
 files.map((fileName) => {
   const content = JSON.parse(
     fs.readFileSync(`./questions/${fileName}`).toString()
   );
+  let counter = 0;
   const updatedFile = content.map((question) => {
     const newOptions = question.options.map((option, i) => {
       const numOfbr = option.split("\r\n");
-      if (option.includes("\r\n\r\n") && i === 3 && fileName.includes("nodejs"))
-        return option.split("\r\n\r\n")[0];
+      if (option.startsWith("\r\n")) {
+        counter += 1;
+      }
       return option;
     });
     return { ...question, options: newOptions };
   });
   totalQuestionsInsert += updatedFile.length;
+  console.log(`${fileName}: ${counter}`);
   fs.writeFileSync(`./questions/${fileName}`, JSON.stringify(updatedFile));
 });
-
 console.log(totalQuestionsInsert);
-
 // const updatedFile = content.map((question) => {
 //   const newOptions = question.options.map((option, i) => {
 //     const numOfbr = option.split("\r\n");
