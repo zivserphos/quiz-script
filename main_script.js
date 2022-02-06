e355448adc79614f619dfd367c8c9c02af36ad29
zivserphos <zivfromisrael@gmail.com>
2022-02-06 13:27:26 +0200

    .

--- main_script.js
+++ main_script.js
@@ -1,12 +1,9 @@
 const path = require("path");
 const fs = require("fs");
 
-const php = JSON.parse(fs.readFileSync("./php1.json").toString());
-const updatedPhp = [];
-php.map((question) => {
-  if (question && question.query) updatedPhp.push(question.query);
-});
-console.log(updatedPhp);
+const jsAns = JSON.parse(fs.readFileSync("./answers/js.json").toString());
+
+console.log(jsAns);
 
 let questionWithCode = 0;
 let totalQuestionsInsert = 0;
@@ -16,19 +13,22 @@ files.map((fileName) => {
   questionWithCode = 0;
   const content = JSON.parse(
     fs.readFileSync(`./questions/${fileName}`).toString()
-  );
-  if (fileName.includes("php")) console.log(content.length);
+  ).slice(25);
+  if (fileName === "javascript.json") console.log(content.length);
   let counter = 0;
   const updatedFile = content.map((question, i) => {
     // const numOfbr = question.options.split("\r\n");
     let updatedQuery = question.query;
-    if (fileName === "php.json" && question.query.startsWith(" ")) {
-      updatedQuery = question.query.slice(1);
+    if (
+      fileName === "javascript.json" &&
+      question.query.includes("What value does this code return?")
+    ) {
+      console.log(i);
     }
 
-    return { ...question, query: updatedQuery };
+    return { ...question };
   });
   totalQuestionsInsert += updatedFile.length;
   // console.log(`${fileName}: ${questionWithCode} questions with code`);
-  fs.writeFileSync(`./questions/${fileName}`, JSON.stringify(updatedFile));
+  // fs.writeFileSync(`./questions/${fileName}`, JSON.stringify(updatedFile));
 });
