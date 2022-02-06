61244032cc4621d6f956469c121b8397412d45f3
zivserphos <zivfromisrael@gmail.com>
2022-02-06 10:58:11 +0200

    updated php

--- main_script.js
+++ main_script.js
@@ -1,6 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 
+const php = JSON.parse(fs.readFileSync("./php1.json").toString());
+const updatedPhp = [];
+php.map((question) => {
+  if (question && question.query) updatedPhp.push(question.query);
+});
+console.log(updatedPhp);
+
 let questionWithCode = 0;
 let totalQuestionsInsert = 0;
 const files = fs.readdirSync("./questions");
@@ -10,18 +17,18 @@ files.map((fileName) => {
   const content = JSON.parse(
     fs.readFileSync(`./questions/${fileName}`).toString()
   );
+  if (fileName.includes("php")) console.log(content.length);
   let counter = 0;
-  const updatedFile = content.map((question) => {
+  const updatedFile = content.map((question, i) => {
     // const numOfbr = question.options.split("\r\n");
     let updatedQuery = question.query;
-    if (question.query === "") {
-      questionWithCode += 1;
-      console.log(fileName);
+    if (fileName === "php.json" && question.query.startsWith(" ")) {
+      updatedQuery = question.query.slice(1);
     }
 
     return { ...question, query: updatedQuery };
   });
   totalQuestionsInsert += updatedFile.length;
-  console.log(`${fileName}: ${questionWithCode} questions with code`);
+  // console.log(`${fileName}: ${questionWithCode} questions with code`);
   fs.writeFileSync(`./questions/${fileName}`, JSON.stringify(updatedFile));
 });
