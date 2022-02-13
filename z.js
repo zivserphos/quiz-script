d28dd33c83fc69cc9cd3bea9f80b58b42d316b62
zivserphos <zivfromisrael@gmail.com>
2022-02-13 16:26:15 +0200

    .

--- z.js
+++ z.js
@@ -1,14 +1,21 @@
-const a = async () =>
-  console.log(
-    await axios.get(
-      "http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all",
-      {
-        proxy: {
-          port: 8115,
-          host: " socks5h://127.0.0.1:9050",
-        },
-      }
-    )
-  );
+const { SocksProxyAgent } = require("socks-proxy-agent");
+const axios = require("axios");
+const proxy = "socks://localHost:9050";
+const agent = new SocksProxyAgent(proxy);
 
-a();
+async function request(baseURL) {
+  try {
+    const client = axios.create({ baseURL, httpAgent: agent });
+    const res = await client.get("/");
+    console.log(res.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error.message);
+    }
+    return "Failed.";
+  }
+}
+
+request(
+  "http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all"
+);
